e38783a6dcc74a8d4efc24eb24a349e1730fb9bc
YeQiu <paulhiram@hotmail.com>
2024-05-29 19:46:22 +0000

    HashFunc

--- src/main/java/HashFunc.java
+++ src/main/java/HashFunc.java
@@ -2,15 +2,22 @@ import java.util.HashMap;
 
 class HashFunch{
 
-
   void wordCounter(String word){
-    HashMap<Character, Integer> wordCounter;
+    HashMap<Character, Integer> letterCounter= new HashMap<Character, Integer>();
 
     for (char ch : word.toCharArray()) {
         System.out.println(ch);
 
-      if(wordCounter)
+      if(letterCounter.get(ch) != null){
+        letterCounter.put(ch, letterCounter.get(ch) + 1);
+      } else {
+        letterCounter.put(ch, 1);
+      }
+      
+      
     }
+
+    System.out.println(letterCounter);
   }
   
 }
