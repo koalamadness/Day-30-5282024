86f32c87655ca706809551214245f9c9eba76bda
YeQiu <paulhiram@hotmail.com>
2024-05-31 20:58:15 +0000

    Add Score

--- src/main/java/PlayerScore.java
+++ src/main/java/PlayerScore.java
@@ -1,15 +1,28 @@
 import java.util.HashMap;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class PlayerScore{
-
-  HashMap<String, ArrayList<Double>> playerScoreMap = new HashMap<String, ArrayList<Double>>();
+  
+  private Scanner scanner = new Scanner(System.in);
+  //private String userName = scanner.nextLine(); 
+  
+  private HashMap<String, ArrayList<Double>> playerScoreMap = new HashMap<String, ArrayList<Double>>();
   
   PlayerScore(HashMap<String, ArrayList<Double>> pSM){
     this.playerScoreMap = pSM;
   }
 
   void addScore(double newScore){
+
+    System.out.println("Enter player to add score");
+    String player = scanner.nextLine();  // Read user input
+
+    if(playerScoreMap.containsKey(player)){
+      playerScoreMap.get(player).add(newScore);
+    } else{
+      System.out.println("Player " + player + "could not be found");
+    }
     
   }
 
